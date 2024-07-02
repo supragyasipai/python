d7a3ae3fa3e98c44ec843ee640a86c803162d68a
supragyasipai <supragya.sipai@gmail.com>
2024-07-01 17:48:01 -0700

    if

--- today.py
+++ today.py
@@ -1,16 +1,18 @@
-data=[12,13,34,65,78,19,18,17,16,15]
-new=[]
-total=len(data)
-x=0
-while x<total:
-    if data[x]%2==0:
-        new.append(data[x])
-print(data)
+# data=[12,13,34,65,78,19,18,17,16,15]
+# new=[]
+# total=len(data)
+# x=0
+# while x<total:
+#     if data[x]%2==0:
+#         new.append(data[x])
+#     x+=1
+# print(new)
 
-# x=1
-# while x<=100:
-#     if x%3==0 and x%5==0:
-#         print(x)
+x=1
+while x<=100:
+    if x%3==0 and x%5==0:
+        print(x)
+    x+=1
 
 # students=['ram','shyam','sitaram','sophia','sophia','gita','madan']
 # x=0
